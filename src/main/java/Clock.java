2c8d0e0696ac355fb92ba5f2063b9f1bd9c9ac91
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-10-30 11:10:51 +0100

    ajout de commentaires pour l'horloge digitale

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -6,9 +6,10 @@
  *                Guidoux Vincent 30 octobre 2018
  *
  * Description  : We implemented a clock with some attribute to handle
- *                delay, gaps... etc We used french for the variables to be consistent with
- *                the lab
+ *                delay, gaps, given delay from other pc(retard) etc We used french for the variables to be consistent
+ *                with the lab. It also give a GUI to display the clock
  * Source       : https://www.javacodex.com/Swing/Digital-Clock
+ *                https://fr.wikipedia.org/wiki/Precision_Time_Protocol#D%C3%A9lai_aller
  */
 
 import javax.swing.*;
@@ -24,11 +25,17 @@ public class Clock {
 
     private long retard = 0;
 
-    private long delai = 0;
+    private long delai = 0; //ds2m
     private long ecart = 0; //dm2s
 
     private long offset = 0;
 
+    /**
+     * constructs a clock with a given delay and a name
+     *
+     * @param name   : name of the clock
+     * @param retard : given delay, to test the algorithm
+     */
     public Clock(String name, long retard) {
         this.name = name;
         this.retard = retard;
@@ -38,7 +45,7 @@ public class Clock {
 
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame f = new JFrame(name);
-        f.setSize(300,150);
+        f.setSize(300, 150);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new GridLayout(3, 1));
 
@@ -51,46 +58,70 @@ public class Clock {
         f.setVisible(true);
     }
 
+    /**
+     * Constructs a clock with a given name and 0 delay at the starts
+     *
+     * @param name
+     */
     public Clock(String name) {
-        this(name,0);
+        this(name, 0);
     }
 
+    /**
+     * @return the local time of the slave
+     */
     public long getCurrentTime() {
         return System.currentTimeMillis() + retard;
     }
 
+    /**
+     * @return the corrected time with the master
+     */
     public long getCorrectedTime() {
         return getCurrentTime() - offset;
     }
 
-    public void setDelai(long delai) {
+    /**
+     * set the delay, it's the difference that occurs between the slave and the master
+     * based on the emission and transmission.
+     *
+     * @param delai : delay calculated at the second part.
+     */
+    public synchronized void setDelai(long delai) {
         this.delai = delai;
         updateOffset();
     }
 
-    public long getEcart() {
-        return ecart;
-    }
-
-    public void setEcart(long ecart) {
+    /**
+     * set the gap between the slave and the master.
+     *
+     * @param ecart : gap between the slave and the master
+     */
+    public synchronized void setEcart(long ecart) {
         this.ecart = ecart;
         updateOffset();
     }
 
-    public long getDelai() {
-        return delai;
-    }
-
-    private void updateOffset(){
-
+    /**
+     * Update the offset
+     */
+    private synchronized void updateOffset() {
+        //d = (dm2s + ds2m) / 2
         long d = (ecart + delai) / 2;
+        //Δt = dm2s - d
         this.offset = ecart - d;
     }
 
+    /**
+     * @return the difference between slave and master
+     */
     public long getOffset() {
         return offset;
     }
 
+    /**
+     * A clock label that display the date, not implemented by us.
+     */
     class ClockLabel extends JLabel implements ActionListener {
 
         final String type;
@@ -125,6 +156,7 @@ public class Clock {
         }
 
         public void actionPerformed(ActionEvent ae) {
+            //We just change that line
             Date d = new Date(getCorrectedTime());
             setText(sdf.format(d));
         }
